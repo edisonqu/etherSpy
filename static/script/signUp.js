d8519efe4e9612995d4fe2baaa1fb0952583de8e
edison <edisonquu@gmail.com>
2022-10-30 05:34:24 -0400

    changed hardcoded buttons

--- static/script/signUp.js
+++ static/script/signUp.js
@@ -1,6 +1,6 @@
 function takeValue(event) {
     event.preventDefault()
-    window.location.href="http://127.0.0.1:5000/thankyou";
+    window.location.href="/thankyou";
 
 
     var blockchainAddress = document.getElementById("blockchain").value
